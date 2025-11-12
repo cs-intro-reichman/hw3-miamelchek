e2e651ec3b15b3564f004a714dbaa67dd1a3ab78
Mia Melchek <miamelchek@Mias-MacBook-Air.local>
2025-11-12 22:08:53 +0200

    Updated 1  3

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,70 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String word1 = preProcess(str1);
+		String word2 = preProcess(str2);
+		char letter1, letter2;
+		// אם הם לא באותו האורך הם לא יכולות להיות אנגרמות
+		if (word1.length() != word2.length()) {
+			return false;
+		}
+			while (word1.length() >0) {
+				
+				letter1 = word1.charAt(0);
+				boolean isEqual = false;
+				
+				if (letter1 == ' ') {
+					word1 = word1.substring(1); // להסיר את הרווח מההתחלה
+					continue;                   // ולהמשיך לסיבוב הבא
+				}	
+
+				for(int j = 0; j < word2.length();j++){
+				letter2 = word2.charAt(j);
+				if (letter2 == ' ') continue;
+					if (letter1 == letter2) {
+						word2 = word2.substring(0, j) + word2.substring(j + 1);
+						word1 = word1.substring(1);
+						isEqual = true;
+						break;
+					}
+				}
+				// במידה ולא נמצאה בכלל האות האשונה אין סיבה להמשיך כי זה לא אנגרמה
+				if (!isEqual) {
+					return false;
+				}
+			}	
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String abcCheck = " abcdefghijklmnopqrstuvwxyz";
+		String newWord = "";
+		String lowerCase = str.toLowerCase();
+		for(int i = 0;i < lowerCase.length(); i++ ){
+			char letter = lowerCase.charAt(i);
+			if (abcCheck.indexOf(letter) != -1) {
+				newWord = newWord + letter;
+			}
+		}
+		return newWord;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String word = preProcess(str);
+		String randWord = "";
+
+		while (word.length() > 0) {
+			int place = (int)(Math.random() * word.length());
+        	char s = word.charAt(place);
+        	randWord += s; // לביצועים עדיף StringBuilder, אבל זה עובר לתרגיל
+        	word = word.substring(0, place) + word.substring(place + 1);
+		}
+		return randWord;
+	
 	}
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,12 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		double remain = loan;
+		double epsilon = 0.01;
+		for(int i = 0; i < n; i++){
+			remain = (remain - payment) * (1 + rate / 100);
+		}
+		return remain;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +42,28 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		iterationCounter = 0;
+		double payment = loan / n;
+		// בהתחלה נגדיר את הצעד גדוליחסית כדי שהפעולה לא תיקח נצח מדובר במספרים מאוד גדולים
+		double payStep = 10;
+		double balance = endBalance(loan, rate, n, payment);
+		double prevBal   = endBalance(loan, rate, n, payment);
+		while (Math.abs(balance) > epsilon) {
+			if (balance > 0) {
+				payment += payStep;
+			}else{
+				payment -= payStep;
+			}
+			iterationCounter++;
+			balance = endBalance(loan, rate, n, payment);
+			// בודקים אם הסימן התחלף וירדנו יותר מידי ואז אפשר לתקן את הצעד להיות קטן יותר כדי להגיע לטווח הטעות המבוקש
+			if (balance * prevBal < 0) {
+				payStep /= 10.0;                  
+				if (payStep < epsilon) break;  
+			}
+			prevBal = balance;
+		}
+		return payment;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +72,20 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		iterationCounter = 0;
+		double lowPay = loan / n;
+		double hiPay = loan * (1 + rate / 100);
+		double midPay = (hiPay + lowPay) / 2;
+		while (hiPay - lowPay > epsilon) {
+			if (endBalance(loan, rate, n, midPay) < 0) {
+				hiPay = midPay;
+			}
+			else{
+				lowPay = midPay;
+			}
+			iterationCounter++;
+			midPay = (hiPay + lowPay) / 2;
+		}
+		return midPay;
     }
 }
